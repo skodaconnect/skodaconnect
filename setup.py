6f268d545bc63e9e14209eefd47a92720f53bf36
lendy007 <25414128+lendy007@users.noreply.github.com>
2020-11-18 21:12:02 +0100

    1.0.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def local_scheme(version):
 
 setup(
     name='skodaconnect',
-    version='1.0.4',
+    version='1.0.5',
     description='Communicate with Skoda Connect',
     author='lendy007',
     author_email='lendik@gmail.com',
