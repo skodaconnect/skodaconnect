6904a56edc288bc09913a45c0cd903ff454e0cd7
Farfar <faekie@hotmail.com>
2020-12-16 14:09:03 +0100

    Removed references to volkswagencarnet
    	ändrad:        tests/dummy_test.py

--- tests/dummy_test.py
+++ tests/dummy_test.py
@@ -10,11 +10,11 @@ sys.path.insert(0, myPath + '/../')
 
 
 @pytest.mark.asyncio
-async def test_volkswagencarnet():
-    import volkswagencarnet
+async def test_skodaconnect():
+    import skodaconnect
     async with ClientSession() as session:
-        connection = volkswagencarnet.Connection(session, 'test@example.com', 'test_password')
+        connection = skodaconnect.Connection(session, 'test@example.com', 'test_password')
         # if await connection._login():
         if not connection.logged_in:
             return True
-    pytest.fail('Something happend we should have got a False from vw.logged_in')
+    pytest.fail('Something happend we should have got a False from skodaconnect connection.logged_in')
