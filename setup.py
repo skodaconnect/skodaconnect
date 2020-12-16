5ecb53e7f3147abaaef64a1cf700d4fe0b939108
Farfar <faekie@hotmail.com>
2020-12-16 14:07:48 +0100

    Bumped version
    	ändrad:        setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def local_scheme(version):
 
 setup(
     name='skodaconnect',
-    version='1.0.18',
+    version='1.0.20',
     description='Communicate with Skoda Connect',
     author='lendy007',
     author_email='lendik@gmail.com',
