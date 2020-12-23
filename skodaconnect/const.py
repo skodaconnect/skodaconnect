66c19cb9a316847fa43d55e70694f5eedabbe695
Farfar <faekie@hotmail.com>
2020-12-23 10:09:33 +0100

    Added constants 	ny fil:        skodaconnect/const.py

---
+++ skodaconnect/const.py
@@ -0,0 +1,29 @@
+"""Constants for Skoda Connect library."""
+
+BASE_SESSION = 'https://msg.volkswagen.de'
+BASE_AUTH = 'https://identity.vwgroup.io'
+CLIENT_ID = '7f045eee-7003-4379-9968-9355ed2adb06%40apps_vw-dilab_com'
+XCLIENT_ID = '28cd30c6-dee7-4529-a0e6-b1e07ff90b79'
+XAPPVERSION = '3.2.6'
+XAPPNAME = 'cz.skodaauto.connect'
+USER_AGENT = 'okhttp/3.7.0'
+APP_URI = 'skodaconnect://oidc.login/''
+
+HEADERS_SESSION = {
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/json',
+    'Accept-charset': 'UTF-8',
+    'Accept': 'application/json',
+    'X-Client-Id': XCLIENT_ID,
+    'X-App-Version': XAPPVERSION,
+    'X-App-Name': XAPPNAME,
+    'User-Agent': USER_AGENT
+}
+
+HEADERS_AUTH = {
+    'Connection': 'keep-alive',
+    'Accept': 'application/json,text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,\
+        image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'User-Agent': USER_AGENT
+}
