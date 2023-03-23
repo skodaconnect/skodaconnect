bf792d31bb5a5d8fb370227c3bc0d3b0c0ded9f3
Fabrice Devaux <fabrice.devaux@gmail.com>
2023-03-23 21:16:18 +0100

    Get example.py working + read email/password from env

--- skodaconnect/__init__.py
+++ skodaconnect/__init__.py
@@ -4,5 +4,5 @@ skodaconnect - A Python 3 library for interacting with Skoda Connect and Smartli
 For more details and documentation, visit the github page at https://github.com/lendy007/skodaconnect
 """
 
-from skodaconnect.connection import Connection
+#from skodaconnect.connection import Connection
 #from .__version__ import __version__
