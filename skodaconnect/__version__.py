64ed523e6397cdaef74e63bf0bd6f0f983b47c0b
Fabrice Devaux <fabrice.devaux@gmail.com>
2024-04-29 08:07:49 +0200

    Bump version to 1.3.11

--- skodaconnect/__version__.py
+++ skodaconnect/__version__.py
@@ -4,4 +4,4 @@ skodaconnect - A Python 3 library for interacting with Skoda Connect and Smartli
 For more details and documentation, visit the github page at https://github.com/skodaconnect/skodaconnect
 """
 
-__version__ = "1.3.10"
+__version__ = "1.3.11"
